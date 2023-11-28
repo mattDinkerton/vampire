b30d54e7f8f530394a54d4f5b2cee7af7fbf1b5c
ZiroGhoul <143099576+ZiroGhoul@users.noreply.github.com>
2023-11-28 13:11:11 -0600

    Add files via upload

---
+++ vampire-version-0.2/vampire-version-0.2/Very very cleaned up/scripts/heart_icon.gd
@@ -0,0 +1,11 @@
+extends HBoxContainer
+
+@onready var HeartIcon = preload("res://scenes/heart_icon_gui.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ vampire-version-0.2/vampire-version-0.2/Very very cleaned up/scripts/world.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+class_name world
+
+@onready var heartsContainer = $CanvasLayer/heartsContainer
+@onready var player = $player
+@onready var heartsIcon = $CanvasLayer/heart_icon
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	heartsContainer.setMaxHearts(player.hp)
+	heartsContainer.updateHearts(player.hp)
+	player.healthChanged.connect(heartsContainer.updateHearts)
+
+
+
+# Script that enables the pause menu
+
+signal toggle_game_paused(is_paused : bool)
+
+var game_paused : bool = false:
+	get:
+		return game_paused
+	set(value):
+		game_paused = value
+		
+		get_tree().paused = !game_paused
+		emit_signal("toggle_game_paused", game_paused)
+
+
+func _input(event : InputEvent):
+	if(event.is_action_pressed("ui_cancel")):
+		game_paused = !game_paused
