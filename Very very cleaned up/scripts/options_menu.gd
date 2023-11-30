41e5dafa3937a92defdf1dfb7f8c44d1af58f81d
mattDinkerton <125430622+mattDinkerton@users.noreply.github.com>
2023-11-30 15:03:03 -0600

    Delete Very very cleaned up directory

--- Very very cleaned up/scripts/options_menu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_volume_pressed():
-	pass # Replace with function body.
-
-
-func _on_back_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
--- Very very cleaned up/scripts/pause_menu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-@export var world : world
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	hide()
-
